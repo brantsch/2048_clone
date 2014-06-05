2667e9d0cdeba905cb6df8f81fcdea94549cd7d5
Peter Brantsch <peter.brantsch@kabelmail.de>
2014-06-05 15:42:40 +0200

    demonstrate the algorithm

--- src/main/TwoThousandFortyEight.java
+++ src/main/TwoThousandFortyEight.java
@@ -6,7 +6,7 @@ import logic.GameOver;
 
 /**
  * @author Peter Brantsch
- *
+ * 
  */
 public abstract class TwoThousandFortyEight {
 
@@ -16,17 +16,39 @@ public abstract class TwoThousandFortyEight {
 	public static void main(String[] args) {
 		GameLogic theGameLogic = new GameLogic();
 		System.out.println("Welcome to 2048.");
-		for(;;){
-			theGameLogic.place2();
-			theGameLogic.printGrid();
-			try{
+		for (int i=0;i<10;++i) {
+			try {
+				theGameLogic.place2();
+				theGameLogic.printGrid();
+				System.out.println("DOWN");
 				theGameLogic.doMove(Direction.DOWN);
-			}catch(GameOver g){
+				theGameLogic.printGrid();
+				System.out.println();
+
+				theGameLogic.place2();
+				theGameLogic.printGrid();
+				System.out.println("LEFT");
+				theGameLogic.doMove(Direction.LEFT);
+				theGameLogic.printGrid();
+				System.out.println();
+
+				theGameLogic.place2();
+				theGameLogic.printGrid();
+				System.out.println("RIGHT");
+				theGameLogic.doMove(Direction.RIGHT);
+				theGameLogic.printGrid();
+				System.out.println();
+
+				theGameLogic.place2();
+				theGameLogic.printGrid();
+				System.out.println("UP");
+				theGameLogic.doMove(Direction.UP);
+				theGameLogic.printGrid();
+				System.out.println();
+			} catch (GameOver g) {
 				System.err.println("GAME OVER");
 				break;
 			}
-			theGameLogic.printGrid();
-			System.out.println();
 		}
 	}
 
